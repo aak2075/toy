4de5bb1ee6c2e00058bb191a4d3a103d8b5a049f
Gyuseong Lee <lks971003@gmail.com>
2023-07-02 19:27:14 +0900

    feat: test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ## 80포트 실행중이면 죽임
-if lsof -i :80 | grep "LISTEN"
+if lsof -i :9000 | grep "LISTEN"
 then
-  kill "$(lsof -t -i :80)"
+  kill "$(lsof -t -i :9000)"
 fi
 
 nohup java -jar -Dserver.port=9000 build/libs/toy* &
