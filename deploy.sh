ccc583d50fb93a8f4067adff049175ef6d01d972
Gyuseong Lee <lks971003@gmail.com>
2023-07-03 11:23:53 +0900

    feat: test2

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if lsof -i :9000 | grep "LISTEN"
-then
-  echo "kill process"
-  kill "$(lsof -t -i :9000)"
-  sleep 5
+port=9000
+
+pid=$(pgrep -f ":$port")
+
+if [ -z "$pid" ]; then
+  echo "포트 $port에서 실행 중인 프로세스가 없습니다."
+else
+  echo "포트 $port에서 실행 중인 프로세스 종료: $pid"
+  kill "$pid"
 fi
 
 echo "run server"
