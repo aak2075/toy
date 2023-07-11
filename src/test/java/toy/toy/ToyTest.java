55b29ee20de6c5c7dd2b47f80009f94f001b9955
aak2075 <lks971003@gmail.com>
2023-07-11 13:47:18 +0900

    feat: test

--- src/test/java/toy/toy/ToyTest.java
+++ src/test/java/toy/toy/ToyTest.java
@@ -3,13 +3,17 @@ package toy.toy;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ToyTest {
 
     @Test
     void sum() {
         Toy toy = new Toy();
-        assertThat(toy.sum(2,3)).isEqualTo(5);
+        assertThat(toy.sum(2, 3)).isEqualTo(5);
+    }
+
+    @Test
+    void fail() {
+        assertThat(0).isEqualTo(1);
     }
 }
